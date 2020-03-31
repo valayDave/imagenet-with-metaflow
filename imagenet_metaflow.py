1871c22a298d4693562d543224f19050a7a37c7d
Valay Dave <valaygaurang@gmail.com>
2020-03-31 04:00:59 -0700

    Removed Dataset from the File Input. Added foreach split.

--- imagenet_metaflow.py
+++ imagenet_metaflow.py
@@ -1,4 +1,4 @@
-from metaflow import FlowSpec, step, Parameter,IncludeFile,kube,environment
+from metaflow import FlowSpec, step, Parameter,IncludeFile,kube,environment,S3
 from constants import supported_models
 import os
 
@@ -26,24 +26,24 @@ class ImageNetExperimentationFlow(FlowSpec):
     '''
     A Flow to experiment and bench mark multiple ML Models in Parallel using PyTorch and Metaflow.
     '''
-    zipped_dataset = IncludeFile('zipped_dataset', default=script_path('./tiny-imagenet-200.zip'),
-                    help='path to ZIPPED dataset',is_text=False)
+    # zipped_dataset = IncludeFile('zipped_dataset', default=script_path('./tiny-imagenet-200.zip'),
+    #                 help='path to ZIPPED dataset',is_text=False)
     
     zipped_dataset_name = Parameter('zipped_dataset_name',default='tiny-imagenet-200',
                             help='Name of folder Which is created when dataset is Untared')
     
-    arch = Parameter('arch', metavar='ARCH', default='resnet18',
-                        help='model architecture: ' +
-                            ' | '.join(supported_models) +
-                            ' (default: resnet18)')
+    # arch = Parameter('arch', metavar='ARCH', default='resnet18',
+    #                     help='model architecture: ' +
+    #                         ' | '.join(supported_models) +
+    #                         ' (default: resnet18)')
 
-    workers = Parameter('workers', default=6, type=int, metavar='N',
+    workers = Parameter('workers', default=3, type=int, metavar='N',
                         help='number of data loading workers (default: 4)')
     epochs = Parameter('epochs', default=1, type=int, metavar='N',
                         help='number of total epochs to run')
     start_epoch = Parameter('start_epoch', default=0, type=int, metavar='N',
                         help='manual epoch number (useful on restarts)')
-    batch_size = Parameter('batch_size', default=256, type=int,
+    batch_size = Parameter('batch_size', default=64, type=int,
                         metavar='N',
                         help='mini-batch size (default: 256), this is the total '
                             'batch size of all GPUs on the current node when '
@@ -95,9 +95,16 @@ class ImageNetExperimentationFlow(FlowSpec):
         # todo : define Hyper Param Search Class
         self.trained_on_gpu = False
         self.used_num_gpus = 0 
-        self.next(self.train_model)
+        self.training_models = [    
+            'resnet101',
+            'resnet152',
+            'resnet18',
+            'resnet34',
+            'resnet50',
+        ]
+        self.next(self.train_model,foreach='training_models')
 
-    @kube(cpu=3,memory=20000,gpu=1,image='anibali/pytorch:cuda-10.1')
+    @kube(cpu=4,memory=40000,gpu=4,image='anibali/pytorch:cuda-10.1')
     @environment(vars={"LC_ALL":"C.UTF-8","LANG":"C.UTF-8"})
     @step
     def train_model(self):
@@ -106,6 +113,8 @@ class ImageNetExperimentationFlow(FlowSpec):
         import random 
         import imagenet_pytorch
         import json
+        with S3() as s3:
+            zipped_dataset = s3.get("s3://metaflow-experiments-datastore/metaflow_store/Datasets/tiny-imagenet-200.zip").blob
         # Create Directory for deloading dataset. 
         random_hex = str(hex(random.randint(0,16777215)))
         self.dataset_final_path = script_path('dataset-'+random_hex)
@@ -113,18 +122,23 @@ class ImageNetExperimentationFlow(FlowSpec):
 
         # Create the Directory for the dataset using Zip Provided as the input dataset.
         zipdata = BytesIO()
-        zipdata.write(self.zipped_dataset)
+        zipdata.write(zipped_dataset)
         dataset_zip_file = ZipFile(zipdata)
-        
+        # Set Arch here for parallel distributed training.
+        self.arch = self.input
         # Extract the dataset 
         dataset_zip_file.extractall(self.dataset_final_path)
-        print("Extracted Dataset")
+        print("Extracted Dataset. Now Training :",self.arch)
         self.dataset_final_path = os.path.join(self.dataset_final_path,self.zipped_dataset_name)
         results = imagenet_pytorch.start_training_session(self)
         # Need to save like this otherwise there are Pickle Serialisation problems. 
         self.epoch_histories = json.loads(json.dumps(results))
-        self.next(self.end)
+        self.next(self.join)
 
+    @step
+    def join(self,inputs):
+        self.history = [{'param':input_val.arch,'history':input_val.epoch_histories} for input_val in inputs]
+        self.next(self.end)
 
     @step
     def end(self):
